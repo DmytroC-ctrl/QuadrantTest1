985d9b1fa7380e12a14570d672b81982f860f924
DmytroC-ctrl <70591462+DmytroC-ctrl@users.noreply.github.com>
2020-09-17 16:48:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quadrant
+{
+    public class QuadrantCalculator
+    {
+
+
+        public string DefinitionQuadrant(int x, int y)
+        {
+            string quadrant = x >= 0 ? (y >= 0 ? "quadrant1" : "quadrant4") : (y >= 0 ? "quadrant2" : "quadrant3");
+            return quadrant;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите координату х- ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Введите координату y- ");
+            int y = int.Parse(Console.ReadLine());
+            //Console.WriteLine(DefinitionQuadrant(x, y));
+
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Quadrant;
+using NUnit.Framework;
+
+namespace QuandrantTest1
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        private QuadrantCalculator quadrantCalculator;
+
+        [OneTimeSetUp]
+        public void OnTestStarted()
+        {
+            quadrantCalculator = new QuadrantCalculator();
+        }
+        [Test]
+        public void TestQuadrant1()
+        {
+            int x = 4;
+            int y = 1;
+
+            string acualQuadrant = quadrantCalculator.DefinitionQuadrant(x, y);
+
+            string expectedQuadrant = "quadrant1";
+            Assert.AreEqual(expectedQuadrant, acualQuadrant);
+
+        }
+        [TestCase(-2, 3, "quadrant2")]
+        [TestCase(-2, -3, "quadrant3")]
+        [TestCase(2, -3, "quadrant4")]
+        [TestCase(0, 3, "quadrant1")]
+        [TestCase(-2, 0, "quadrant2")]
+        [TestCase(0, 0, "quadrant1")]
+        public void TestQuadrant(int x, int y, string expectedQuandrante)
+        {
+
+            string acualQuadrant = quadrantCalculator.DefinitionQuadrant(x, y);
+
+            Assert.AreEqual(expectedQuandrante, acualQuadrant);
+
+        }
+    }
+}
